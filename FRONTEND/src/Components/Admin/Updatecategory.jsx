172660f9ee6c56d19219b600a3976b6c7ce74362
VedTiwari278 <148948448+VedTiwari278@users.noreply.github.com>
2025-07-26 10:17:17 +0530

    commit

--- FRONTEND/src/Components/Admin/Updatecategory.jsx
+++ FRONTEND/src/Components/Admin/Updatecategory.jsx
@@ -29,8 +29,9 @@ const Updatecategory = () => {
         );
       }
     };
-
-    fetchCategory();
+    setInterval(() => {
+      fetchCategory();
+    }, 1000);
   }, [id]);
 
   const handleSubmit = async (e) => {
