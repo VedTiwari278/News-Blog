e57847ac737aff3ed30c22a74bf7aeb7a3ab167c
VedTiwari278 <148948448+VedTiwari278@users.noreply.github.com>
2025-07-26 12:18:31 +0530

    commit

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -35,6 +35,8 @@ const App = () => {
       <NavBar></NavBar>
       <Routes>
         {/* <Route path="*" element={<Index />} /> */}
+
+
         <Route path="/" element={<Index />} />
         <Route path="/admin" element={<Home />} />
         <Route path="/admin/category" element={<Category />} />
