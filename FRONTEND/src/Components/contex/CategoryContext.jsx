2a3096923a32301f6f212c82a086deadd8f75b6e
VedTiwari278 <148948448+VedTiwari278@users.noreply.github.com>
2025-07-28 18:36:00 +0530

    c

--- FRONTEND/src/Components/contex/CategoryContext.jsx
+++ FRONTEND/src/Components/contex/CategoryContext.jsx
@@ -6,9 +6,13 @@ export const CategoryProvider = ({ children }) => {
   const [categories, setCategories] = useState([]);
 
   const fetchCategories = async () => {
-    const res = await fetch("https://news-blog-abh6.vercel.app/admin/get-categories");
-    const data = await res.json();
-    setCategories(data);
+    try {
+      const res = await fetch("https://news-blog-abh6.vercel.app/admin/get-categories");
+      const data = await res.json();
+      setCategories(data.data); // Make sure to access .data properly
+    } catch (err) {
+      console.error("Error fetching categories:", err);
+    }
   };
 
   useEffect(() => {
