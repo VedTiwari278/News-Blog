3371c8567a69fdb5ca6ffb76a0cbc8edff926e36
VedTiwari278 <148948448+VedTiwari278@users.noreply.github.com>
2026-02-19 00:27:29 +0530

    Pushing code

--- blogify-admin-dashboard/src/config/index.js
+++ blogify-admin-dashboard/src/config/index.js
@@ -1,5 +1,3 @@
-const NODE_ENV = "production";
 
-export const BASE_URL = NODE_ENV
-  ? import.meta.env.PROD_VITE_BASE_URL
-  : import.meta.env.DEV_VITE_BASE_URL;
+
+export const BASE_URL = "https://news-blog-8wof.vercel.app/api/v1";
