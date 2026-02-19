af57e74534e79122224181b5c6ce6380b9985532
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2026-02-20 00:16:19 +0530

    u

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,40 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import morgan from "morgan";
-// import { limiter } from "./ratelimiter/index.js";
-// import { VERSION } from "./config/index.js";
-// import authRouter from "./Router/auth.routes.js";
-// import errorHandler from "./middleware/errormiddleware.js";
-// import { blogRouter } from "./Router/blog.routes.js";
-
-// const app = express();
-// app.use(morgan("dev"));
-
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:5173",
-//       "http://localhost:3000",
-//       "https://news-blog-8wof.vercel.app",
-//       "https://news-blog-n7tv.vercel.app",
-//     ],
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   }),
-// );
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(limiter);
-
-// app.get("/", (req, res) => {
-//   res.send("API is running...");
-// });
-
-// app.use(`/api/${VERSION}/auth/`, authRouter);
-// app.use(`/api/${VERSION}/blog/`, blogRouter);
-
-// app.use(errorHandler);
-// export default app;
-
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
@@ -45,8 +8,8 @@ import errorHandler from "./middleware/errormiddleware.js";
 import { blogRouter } from "./Router/blog.routes.js";
 
 const app = express();
+app.use(morgan("dev"));
 
-// ✅ Correct CORS
 app.use(
   cors({
     origin: [
@@ -56,15 +19,9 @@ app.use(
       "https://news-blog-n7tv.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
-
-// ✅ Handle preflight explicitly
-app.options("*", cors());
-app.use(morgan("dev"));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(limiter);
@@ -73,9 +30,8 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.use(`/api/${VERSION}/auth`, authRouter);
-app.use(`/api/${VERSION}/blog`, blogRouter);
+app.use(`/api/${VERSION}/auth/`, authRouter);
+app.use(`/api/${VERSION}/blog/`, blogRouter);
 
 app.use(errorHandler);
-
 export default app;
--- blogify-admin-dashboard/src/config/index.js
+++ blogify-admin-dashboard/src/config/index.js
@@ -1,3 +1,4 @@
-
-
-export const BASE_URL = "https://news-blog-8wof.vercel.app/api/v1";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://news-blog-8wof.vercel.app/api/v1"
+    : "http://localhost:8000/api/v1";
